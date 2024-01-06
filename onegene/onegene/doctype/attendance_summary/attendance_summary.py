13e95e47d9c7f03a02d497b423f1275c1b8f64e1
SAHAYASTERWIN <sahayasterwin18@gmail.com>
2024-01-06 14:05:26 +0530

    feat:Required Material List Append in UI Dialog

--- onegene/onegene/doctype/attendance_summary/attendance_summary.py
+++ onegene/onegene/doctype/attendance_summary/attendance_summary.py
@@ -19,10 +19,11 @@ def get_data_system(emp,from_date,to_date):
     emp_details = frappe.db.get_value('Employee',emp,['employee_name','department'])
 
     data = "<table class='table table-bordered=1'>"
-    data += "<tr><td style = 'border: 1px solid black;background-color:#ffedcc'><b>ID</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'colspan=2><b>%s</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Name</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;' colspan=2><b>%s</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Dept</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;' colspan=2><b>%s</b></td></tr>"%(emp,emp_details[0],emp_details[1])
+    data += "<tr><td style = 'border: 1px solid black;background-color:#FFA500'><b>ID</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'colspan=2><b>%s</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Name</b></td><td style = 'border: 1px solid black;background-color:#FFA500;' colspan=2><b>%s</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Dept</b></td><td style = 'border: 1px solid black;background-color:#FFA500;' colspan=2><b>%s</b></td></tr>"%(emp,emp_details[0],emp_details[1])
     data += "<tr><td style = 'border: 1px solid black;'colspan=6><b><center>Attendance</center></b></td><td style = 'border: 1px solid black;'colspan=3><b><center>Overtime</center></b></td><tr>"
-    data += "<tr><td style = 'border: 1px solid black;background-color:#ffedcc'><b>Date</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Day</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Working</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>In Time</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Out Time</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Status</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Start Time</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>End Time</b></td><td style = 'border: 1px solid black;background-color:#ffedcc;'><b>Total Hrs</b></td></tr>"
-   
+    data += "<tr><td style = 'border: 1px solid black;background-color:#FFA500'><b>Date</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Day</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Working</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>In Time</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Out Time</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Status</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Start Time</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>End Time</b></td><td style = 'border: 1px solid black;background-color:#FFA500;'><b>Total Hrs</b></td></tr>"
+    overtime_hours = 0
+    overtime_hours_float = 0
     for date in dates:
         dt = datetime.strptime(date,'%Y-%m-%d')
         d = dt.strftime('%d-%b')
@@ -30,8 +31,18 @@ def get_data_system(emp,from_date,to_date):
         holiday  = check_holiday(date)
         in_time = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'in_time') or ''
         out_time = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'out_time') or ''
-
-        data += "<tr><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td></tr>"%(d,day,holiday or 'W',format_datetime(in_time) or '',format_datetime(out_time) or '')
+        shift = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'shift') or ''
+        custom_from_time = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'custom_from_time') or ''
+        custom_to_time = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'custom_to_time') or ''
+        custom_overtime_hours = frappe.db.get_value('Attendance' ,{'employee':emp,"attendance_date":date},'custom_overtime_hours') or ''
+        try:
+            overtime_hours_float = float(custom_overtime_hours)
+        except ValueError:
+            overtime_hours_float = 0.0 
+        frappe.errprint(type(overtime_hours_float))
+        overtime_hours += overtime_hours_float
+        data += "<tr><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td><td style = 'border: 1px solid black;'>%s</td></tr>"%(d,day,holiday or 'W',format_datetime(in_time) or '',format_datetime(out_time) or '',shift or " ",custom_from_time or " ",custom_to_time or " ",custom_overtime_hours or "")
+    data += "<tr><td colspan=6 style = 'border: 1px solid black;background-color:#FFA500'></td><td colspan=2 style = 'border: 1px solid black;background-color:#FFA500'><center><b>TOTAL</b></center></td><td style = 'border: 1px solid black;background-color:#FFA500'><b>%s</b></td></tr>"%(overtime_hours)
     data += "</table>"
     return data
 
