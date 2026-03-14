db931d1c9ff568958210c92e03836563de9f0b70
Amar Karthick P <karthickpamar@gmail.com>
2026-03-14 11:35:11 +0530

    TS19295 - Refresh error on Production Entry

--- onegene/onegene/stock_entry.py
+++ onegene/onegene/stock_entry.py
@@ -15,75 +15,11 @@ def update_stock_qty_in_job_card(doc, method):
 def enqueue_update_stock_qty_in_job_card(doc):
 	try:
 		for stock_entry_details in doc.items:
-			job_cards = frappe.db.get_all("Job Card Required Material Operation", {"item_code": stock_entry_details.item_code}, pluck="parent")
+			job_cards = frappe.db.get_all("Job Card Required Material Operation", {"item_code": stock_entry_details.item_code, "parent": ["!=", doc.job_card]}, pluck="parent")
 			for job_card_name in job_cards:
 				job_card = frappe.get_doc("Job Card", job_card_name)
-
-				# if job_card.status in ["Submitted", "Cancelled", "Completed"]:
-				# 	return {"updated": False}
-					
-				
-				# possible = 0
-
-				# for item in job_card.items:
-				# 	previous_possible = job_card.for_quantity - job_card.total_completed_qty
-				# 	item_warehouse = frappe.db.get_value("Item", item.item_code, "custom_warehouse")
-				# 	warehouse = (
-				# 		item_warehouse
-				# 		if item_warehouse in ["Semi Finished Goods - WAIP", "Finished Goods - WAIP"]
-				# 		else "Shop Floor - WAIP"
-				# 	)
-
-				# 	required_qty = flt(item.required_qty) / flt(job_card.for_quantity)
-				# 	total_required_qty = flt(required_qty) * flt(job_card.for_quantity)
-
-				# 	stock_qty = flt(frappe.db.get_value("Bin", {
-				# 		"item_code": item.item_code,
-				# 		"warehouse": warehouse
-				# 	}, "actual_qty")) or 0
-
-				# 	available_qty = flt(frappe.db.get_value("Bin", {
-				# 		"item_code": item.item_code,
-				# 		"warehouse": "Work In Progress - WAIP",
-				# 	}, "actual_qty")) or 0
-
-				# 	available_qty = min(available_qty, total_required_qty)
-				# 	actual_qty = available_qty + stock_qty
-				# 	actual_possible = min(actual_qty, total_required_qty) # in Required Qty
-				# 	actual_possible = actual_possible / required_qty # in Manufacturing Qty
-     
-				# 	job_card.append("custom_rm_availability", {
-				# 		"item_code": item.item_code,
-				# 		"item_name": frappe.db.get_value("Item", item.item_code, "item_name"),
-				# 		"actual_qty": actual_qty,
-				# 		"available_qty": available_qty,
-				# 		"stock_qty": stock_qty,
-				# 		"rate": 0,
-				# 		"warehouse": warehouse,
-				# 	})
-				# 	job_card.append("custom_required_material_for_operation", {
-				# 		"item_code": item.item_code,
-				# 		"item_name": frappe.db.get_value("Item", item.item_code, "item_name"),
-				# 		"required_qty": required_qty,
-				# 		"total_required_qty": total_required_qty,
-				# 		"stock_qty": stock_qty,
-				# 		"available_qty": actual_qty,
-				# 		"possible_production": actual_qty / required_qty,
-				# 		"consumption_qty": 0,
-				# 		"actual_possible": actual_possible
-				# 	})
-				# 	possible = min(previous_possible, (actual_qty / required_qty))
-
-				# if len(job_card.items) > 0:
-				# 	possible_qty = round(possible)
-				# else:
-				# 	possible_qty = (
-				# 		job_card.custom_possible_qty - job_card.custom_processed_qty
-				# 		if job_card.custom_possible_qty - job_card.custom_processed_qty > 0
-				# 		else 0
-				# 	)
-				# job_card.custom_possible_qty = possible_qty
 				job_card.save(ignore_permissions=True)
+				frappe.errprint("dweknfewkjnfkjwenkjfwen")
 	except Exception as e:
 		frappe.log_error(frappe.get_traceback(), "Error in enqueue_update_stock_qty_in_job_card")
 
